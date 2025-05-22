8166202b3944879ac6a21204c7830fe6ed2cd87c
sej-chaugule1 <sejal.chaugule@education.nsw.gov.au>
2025-05-22 21:32:30 +1000

    Continued working on the login page
    Didn't like how the login/registration page looked before so i changed it around, resulting in changes having to be made in my wireframes/storyboards.

--- myPWA/frontend/script.js
+++ myPWA/frontend/script.js
@@ -1,29 +1,31 @@
-function validateLogin() {
-    const correctUsername = "Software";
-    const correctPassword = "Engineering";
-    
-    const enteredUsername = document.getElementById("user").value;
-    const enteredPassword = document.getElementById("pass").value;
-    const errorMessage = document.getElementById("error-message");
-    const emptyMessage = document.getElementById("empty-message");
-    
-    errorMessage.style.display = "none";
-    emptyMessage.style.display = "none";
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+const wrapper = document.querySelector(".wrapper");
+const loginTitle = document.querySelector(".title-login");
+const registerTitle = document.querySelector(".title-register");
+const signUpBtn = document.querySelector("#SignUpBtn");
+const signInBtn = document.querySelector("#SignInBtn");
 
-errorMessage.style.display = "none";
-emptyMessage.style.display = "none";
-
-if (enteredUsername === "" || enteredPassword === "") {
-    emptyMessage.style.display = "block";
-    return false; 
+function loginFunction(){
+    loginForm.style.left = "50%";
+    loginForm.style.opacity = 1;
+    registerForm.style.left = "150%";
+    registerForm.style.opacity = 0;
+    wrapper.style.height = "500px";
+    loginTitle.style.top = "50%";
+    loginTitle.style.opacity = 1;
+    registerTitle.style.top = "50px";
+    registerTitle.style.opacity = 0;
 }
 
-if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
-    window.location.href = "Penny-wise.html";
-    return false; 
-} else {
-    errorMessage.style.display = "block"; 
-    return false; 
-}
-
+function registerFunction(){
+    loginForm.style.left = "-50%";
+    loginForm.style.opacity = 0;
+    registerForm.style.left = "50%";
+    registerForm.style.opacity = 1;
+    wrapper.style.height = "500px";
+    loginTitle.style.top = "-60px";
+    loginTitle.style.opacity = 0;
+    registerTitle.style.top = "50%";
+    registerTitle.style.opacity = 1;
 }
